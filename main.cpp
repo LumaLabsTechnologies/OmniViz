ef016654ee1ab6e9fc26db4045cc5235998b7d31
Elie Michel <elie.michel@exppad.com>
2024-11-12 22:56:08 +0100

    Upgrade to step 043

--- main.cpp
+++ main.cpp
@@ -16,6 +16,7 @@
 #include <iostream>
 #include <cassert>
 #include <vector>
+#include <array>
 
 class Application {
 public:
@@ -30,6 +31,19 @@ public:
 
 	// Return true as long as the main loop should keep on running
 	bool IsRunning();
+
+private:
+	// Internal structures
+	/**
+	 * The same structure as in the shader, replicated in C++
+	 */
+	struct MyUniforms {
+		std::array<float, 4> color;  // or float color[4]
+		float time;
+		float _pad[3];
+	};
+	// Have the compiler check byte alignment
+	static_assert(sizeof(MyUniforms) % 16 == 0);
 
 private:
 	WGPUTextureView GetNextSurfaceTextureView();
@@ -231,8 +245,9 @@ void Application::MainLoop() {
 	glfwPollEvents();
 
 	// Update uniform buffer
-	float t = static_cast<float>(glfwGetTime()); // glfwGetTime returns a double
-	wgpuQueueWriteBuffer(queue, uniformBuffer, 0, &t, sizeof(float));
+	float time = static_cast<float>(glfwGetTime());
+	// Only update the 1-st float of the buffer
+	wgpuQueueWriteBuffer(queue, uniformBuffer, offsetof(MyUniforms, time), &time, sizeof(float));
 
 	// Get the next target texture view
 	WGPUTextureView targetView = GetNextSurfaceTextureView();
@@ -456,9 +471,11 @@ void Application::InitializePipeline() {
 	// The binding index as used in the @binding attribute in the shader
 	bindingLayout.binding = 0;
 	// The stage that needs to access this resource
-	bindingLayout.visibility = WGPUShaderStage_Vertex;
+	bindingLayout.visibility = WGPUShaderStage_Vertex | WGPUShaderStage_Fragment;
+	//                         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ This changed
 	bindingLayout.buffer.type = WGPUBufferBindingType_Uniform;
-	bindingLayout.buffer.minBindingSize = 4 * sizeof(float);
+	bindingLayout.buffer.minBindingSize = sizeof(MyUniforms);
+	//                                    ^^^^^^^^^^^^^^^^^^ This was 4 * sizeof(float)
 
 	// Create a bind group layout
 	WGPUBindGroupLayoutDescriptor bindGroupLayoutDesc{};
@@ -560,7 +577,8 @@ void Application::InitializeBuffers() {
 	// Create uniform buffer (reusing bufferDesc from other buffer creations)
 	// The buffer will only contain 1 float with the value of uTime
 	// then 3 floats left empty but needed by alignment constraints
-	bufferDesc.size = 4 * sizeof(float);
+	bufferDesc.size = sizeof(MyUniforms);
+	//                ^^^^^^^^^^^^^^^^^^ This was 4 * sizeof(float)
 
 	// Make sure to flag the buffer as BufferUsage::Uniform
 	bufferDesc.usage = WGPUBufferUsage_CopyDst | WGPUBufferUsage_Uniform;
@@ -568,8 +586,11 @@ void Application::InitializeBuffers() {
 	bufferDesc.mappedAtCreation = false;
 	uniformBuffer = wgpuDeviceCreateBuffer(device, &bufferDesc);
 
-	float currentTime = 1.0f;
-	wgpuQueueWriteBuffer(queue, uniformBuffer, 0, &currentTime, sizeof(float));
+	// Upload the initial value of the uniforms
+	MyUniforms uniforms;
+	uniforms.time = 1.0f;
+	uniforms.color = { 0.0f, 1.0f, 0.4f, 1.0f };
+	wgpuQueueWriteBuffer(queue, uniformBuffer, 0, &uniforms, sizeof(MyUniforms));
 }
 
 void Application::InitializeBindGroups() {
@@ -584,7 +605,8 @@ void Application::InitializeBindGroups() {
 	// multiple uniform blocks.
 	binding.offset = 0;
 	// And we specify again the size of the buffer.
-	binding.size = 4 * sizeof(float);
+	binding.size = sizeof(MyUniforms);
+	//             ^^^^^^^^^^^^^^^^^^ This was 4 * sizeof(float)
 
 	// A bind group contains one or multiple bindings
 	WGPUBindGroupDescriptor bindGroupDesc{};
