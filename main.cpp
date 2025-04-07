fca5201763ab26fa599350abfedc3a84c4d7e196
Elie Michel <elie.michel@exppad.com>
2025-04-07 22:44:01 +0200

    Remove redundant call to glfwGetWGPUSurface

--- main.cpp
+++ main.cpp
@@ -94,7 +94,6 @@ bool Application::Initialize() {
 	
 	// Get adapter
 	std::cout << "Requesting adapter..." << std::endl;
-	surface = glfwGetWGPUSurface(instance, window);
 	RequestAdapterOptions adapterOpts = {};
 	adapterOpts.compatibleSurface = surface;
 	Adapter adapter = instance.requestAdapter(adapterOpts);
